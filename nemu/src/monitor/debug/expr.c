99bc4326b4b07d9613b2c207f9cc7d10697b1f9c
tracer-ics2017 <tracer@njuics.org>
2025-03-12 20:31:23 -0700

    >  compile 161220000 yuan Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  20:31:23 up 17:53,  1 user,  load average: 0.16, 0.42, 0.40 8af21183ced318fbca2238b9dfc671dfe46ccd42

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -327,10 +327,7 @@ uint32_t expr(char *e, bool *success) {
   }
 
   /* TODO: Insert codes to evaluate the expression. */
-  for (int i = 0; i < nr_token; i++) {
-
-  }
-
+ 
   *success = true;
   return eval(0, nr_token - 1);
 
