beeccdfb301f6bd287aff4700ff726b8914e5700
tracer-ics2017 <tracer@njuics.org>
2025-03-24 22:22:14 -0700

    >  compile 2210983 yuan Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  22:22:14 up 1 day,  8:21,  1 user,  load average: 1.04, 0.56, 0.42 890541be21add228f2b0b99858f3d8fdb97d01c1

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -1,8 +1,24 @@
 #include "cpu/exec.h"
 
+//data-mov.c
 make_EHelper(mov);
+make_EHelper(push);
+make_EHelper(pop);
 
+//prefix.c
 make_EHelper(operand_size);
 
+//special.c
 make_EHelper(inv);
 make_EHelper(nemu_trap);
+
+// logic.c
+make_EHelper(xor);
+
+// control.c
+make_EHelper(call);
+make_EHelper(ret);
+
+
+// arith.c
+make_EHelper(sub);
