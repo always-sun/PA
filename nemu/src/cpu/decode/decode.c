db3c0a62a524ea98543d424da26448212c30f25a
tracer-ics2017 <tracer@njuics.org>
2025-03-26 22:05:13 -0700

    >  compile 2210983 yuan Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  22:05:13 up 2 days,  2:08,  1 user,  load average: 0.49, 1.24, 1.56 d5ec3b4f25403fce2cac0bc5d1e425f9ff9c36cd

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -305,12 +305,21 @@ make_DHelper(out_a2dx) {
 #endif
 }
 
-void operand_write(Operand *op, rtlreg_t* src) {
+/*void operand_write(Operand *op, rtlreg_t* src) {
   if (op->type == OP_TYPE_REG) { rtl_sr(op->reg, op->width, src); }
   else if (op->type == OP_TYPE_MEM) { rtl_sm(&op->addr, op->width, src); }
   else { assert(0); }
-
-  printf("[operand_write] reg: %d (width=%d), value = 0x%08x\n",
-       op->reg, op->width, *src);
-
+}*/
+void operand_write(Operand *op, rtlreg_t* src) {
+  if (op->type == OP_TYPE_REG) {
+    rtl_sr(op->reg, op->width, src);
+    printf("[operand_write] write to REG %d (width=%d), value = 0x%08x\n",
+           op->reg, op->width, *src);
+  } else if (op->type == OP_TYPE_MEM) {
+    rtl_sm(&op->addr, op->width, src);
+    printf("[operand_write] write to MEM[0x%08x] (width=%d), value = 0x%08x\n",
+           op->addr, op->width, *src);
+  } else {
+    assert(0);
+  }
 }
