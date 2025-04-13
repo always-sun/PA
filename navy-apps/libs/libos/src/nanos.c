c9c5fa13a46452c3b0550651c0a4f127868c48a0
tracer-ics2017 <tracer@njuics.org>
2025-04-12 23:59:11 -0700

    >  run 2210983 yuan Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  23:59:11 up 3 days,  1:01,  1 user,  load average: 0.38, 0.40, 0.50 9bc7a3024f7b6d9371909aa9827da0179a6f24c2

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -29,6 +29,9 @@ int _write(int fd, void *buf, size_t count){
   return _syscall_(SYS_write, (uintptr_t)fd, (uintptr_t)buf, (uintptr_t)count);
 }
 
+extern char _end;
+static intptr_t brk = (intptr_t)&_end;
+
 void *_sbrk(intptr_t increment){
   intptr_t old_brk = brk;
   intptr_t new_brk = old_brk + increment;
