26de40e94fcf7da32adf13d34c3f856967543999
tracer-ics2017 <tracer@njuics.org>
2025-04-13 01:56:50 -0700

    >  run 2210983 yuan Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  01:56:50 up 3 days,  2:59,  1 user,  load average: 0.74, 0.62, 0.59 91cd562db8edd431fce8e24a8fb16ffc1f14f26

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,6 +1,6 @@
 #include "common.h"
 
-#define DEFAULT_ENTRY ((void *)0x4000000)
+#define DEFAULT_ENTRY ((void *)0x08048000)
 
 extern void* new_page(void);
 extern void ramdisk_read(void* buf, off_t offset, size_t len);
