0140de33ef37b88ac6f3441eb875cf15db745694
tracer-ics2017 <tracer@njuics.org>
2025-04-26 22:39:24 -0700

    >  compile 2210983 yuan Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  22:39:24 up 4 days, 29 min,  1 user,  load average: 0.97, 0.44, 0.33 5d55cd4c2031d4ad772b6fb4f6d1c2be5b5fe652

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -28,11 +28,77 @@ void paddr_write(paddr_t addr, int len, uint32_t data) {
         mmio_write(addr, len, data, r);
 }
 
+// uint32_t vaddr_read(vaddr_t addr, int len) {
+//   return paddr_read(addr, len);
+// }
+
+// void vaddr_write(vaddr_t addr, int len, uint32_t data) {
+//   paddr_write(addr, len, data);
+// }
+
+
+#define PDX(va)     (((uint32_t)(va) >> 22) & 0x3ff)
+#define PTX(va)     (((uint32_t)(va) >> 12) & 0x3ff)
+#define OFF(va)     ((uint32_t)(va) & 0xfff)
+#define PTE_ADDR(pte)   ((uint32_t)(pte) & ~0xfff)
+
+
+// +--------10------+-------10-------+---------12----------+
+// | Page Directory |   Page Table   | Offset within Page  |
+// |      Index     |      Index     |                     |
+// +----------------+----------------+---------------------+
+//  \--- PDX(va) --/ \--- PTX(va) --/\------ OFF(va) ------/
+
+
+paddr_t page_translate(vaddr_t addr, bool iswrite) {
+    if (cpu.cr0.protect_enable && cpu.cr0.paging) {
+        PDE *pgdir = (PDE *)PTE_ADDR(cpu.cr3.val);
+        PDE pde;
+        pde.val = paddr_read((paddr_t)&pgdir[PDX(addr)], 4);
+        Assert(pde.present, "Page directory entry not present, addr = 0x%x", addr);
+
+        PTE *pgtab = (PTE *)PTE_ADDR(pde.val);
+        PTE pte;
+        pte.val = paddr_read((paddr_t)&pgtab[PTX(addr)], 4);
+        Assert(pte.present, "Page table entry not present, addr = 0x%x", addr);
+
+        pde.accessed = 1;
+        pte.accessed = 1;
+        if (iswrite) {
+            pte.dirty = 1;
+        }
+
+        paddr_t paddr = PTE_ADDR(pte.val) | OFF(addr);
+        return paddr;
+    }
+
+    return addr;
+}
+
+
 uint32_t vaddr_read(vaddr_t addr, int len) {
-  return paddr_read(addr, len);
+  if ((((addr) + (len) - 1) & ~PAGE_MASK) != ((addr) & ~PAGE_MASK)) {
+    uint32_t data = 0;
+    for (int i = 0; i < len; i++) {
+      paddr_t paddr = page_translate(addr + i, false);
+      data |= (paddr_read(paddr, 1)) << (8 * i);
+    }
+    return data;
+  } else {
+    paddr_t paddr = page_translate(addr, false);
+    return paddr_read(paddr, len);
+  }
 }
 
 void vaddr_write(vaddr_t addr, int len, uint32_t data) {
-  paddr_write(addr, len, data);
+  if ((((addr) + (len) - 1) & ~PAGE_MASK) != ((addr) & ~PAGE_MASK)) {
+    for (int i = 0; i < len; i++) {
+      paddr_t paddr = page_translate(addr + i, true);
+      paddr_write(paddr, 1, data >> (8 * i));
+    }
+  } else {
+    paddr_t paddr = page_translate(addr, true);
+    paddr_write(paddr, len, data);
+  }
 }
 
