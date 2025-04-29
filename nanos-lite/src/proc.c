eed6c057beb646fa05843da9608a90765284473f
tracer-ics2017 <tracer@njuics.org>
2025-04-29 02:39:56 -0700

    >  run 2210983 yuan Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  02:39:56 up 4 days,  8:33,  1 user,  load average: 0.56, 0.27, 0.23 e17b049a499c2e935f4ee356a7350d722a0e696

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -27,6 +27,15 @@ void load_prog(const char *filename) {
 }
 
 _RegSet* schedule(_RegSet *prev) {
-  
-  return NULL;
+
+  if (current != NULL) {
+    current->tf = prev;
+  }
+
+  current = &pcb[0];  
+  Log("Switching to process with page dir PTR=0x%08x\n", (uint32_t)current->as.ptr);
+
+  _switch(&current->as);
+  return current->tf;
+
 }
