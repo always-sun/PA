ea13d9c37dd9f73a685f8e7a5deb6da4908e3379
tracer-ics2017 <tracer@njuics.org>
2025-04-29 19:13:53 -0700

    >  run 2210983 yuan Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  19:13:53 up 4 days,  9:03,  1 user,  load average: 1.00, 0.67, 0.47 20bf76a28ec888fdcb0344d563b02dbb2fd2b17

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -10,8 +10,8 @@ static _RegSet* do_event(_Event e, _RegSet* r) {
 
     case _EVENT_TRAP:
       printf("event trapped\n");
-      do_syscall(r);
-       printf("event trapped-1\n");
+      //do_syscall(r);
+     // printf("event trapped-1\n");
       return schedule(r);
 
     default: panic("Unhandled event ID = %d", e.event);
